a0257f011874fdbd14aa5bf92b40fbed06cb1cd5
fusho-takahashi <zenryaku@fusho-takahashi.me>
2020-02-03 19:07:06 +0900

    character.js 写経完了

--- stg/011_copy/script/character.js
+++ stg/011_copy/script/character.js
@@ -5,8 +5,12 @@ class Position {
   }
 
   set(x, y) {
-    if(x != null){this.x = x}
-    if(y != null){this.y = y}
+    if (x != null) {
+      this.x = x;
+    }
+    if (y != null) {
+      this.y = y;
+    }
   }
 }
 
@@ -15,15 +19,19 @@ class Character {
     this.ctx = ctx;
     this.position = new Position(x, y);
     this.vector = new Position(0.0, -1.0);
-    this.angle = 270 * Math.PI / 180;
+    this.angle = (270 * Math.PI) / 180;
     this.width = w;
     this.height = h;
     this.life = life;
     this.ready = false;
     this.image = new Image();
-    this.image.addEventListener('load', () => {
-      this.ready = true;
-    }, false);
+    this.image.addEventListener(
+      'load',
+      () => {
+        this.ready = true;
+      },
+      false
+    );
     this.image.src = imagePath;
   }
 
@@ -61,13 +69,7 @@ class Character {
     let offsetX = this.width / 2;
     let offsetY = this.height / 2;
 
-    this.ctx.drawImage(
-      this.image, 
-      -offsetX,
-      -offsetY,
-      this.width,
-      this.height,
-    );
+    this.ctx.drawImage(this.image, -offsetX, -offsetY, this.width, this.height);
     this.ctx.restore();
   }
 }
@@ -104,32 +106,32 @@ class Viper extends Character {
   update() {
     let justTime = Date.now();
 
-    if(this.isComing === true) {
+    if (this.isComing === true) {
       let comingTime = (justTime - this.comingStart) / 1000;
       let y = this.comingStartPosition.y - comingTime * 50;
 
-      if(y <= this.comingEndPosition.y) {
+      if (y <= this.comingEndPosition.y) {
         this.isComing = false;
         y = this.comingEndPosition.y;
       }
 
       this.position.set(this.position.x, y);
 
-      if(justTime % 100 < 50) {
+      if (justTime % 100 < 50) {
         this.ctx.globalAlpha = 0.5;
       }
     } else {
-      if(window.isKeyDown.key_ArrowLeft === true){
+      if (window.isKeyDown.key_ArrowLeft === true) {
         this.position.x -= this.speed; // アローキーの左
       }
-      if(window.isKeyDown.key_ArrowRight === true){
-          this.position.x += this.speed; // アローキーの右
+      if (window.isKeyDown.key_ArrowRight === true) {
+        this.position.x += this.speed; // アローキーの右
       }
-      if(window.isKeyDown.key_ArrowUp === true){
-          this.position.y -= this.speed; // アローキーの上
+      if (window.isKeyDown.key_ArrowUp === true) {
+        this.position.y -= this.speed; // アローキーの上
       }
-      if(window.isKeyDown.key_ArrowDown === true){
-          this.position.y += this.speed; // アローキーの下
+      if (window.isKeyDown.key_ArrowDown === true) {
+        this.position.y += this.speed; // アローキーの下
       }
       let canvasWidth = this.ctx.canvas.width;
       let canvasHeight = this.ctx.canvas.height;
@@ -137,23 +139,69 @@ class Viper extends Character {
       let ty = Math.min(Math.max(this.position.y, 0), canvasHeight);
       this.position.set(tx, ty);
 
-      if(window.isKeyDown.key_z === true) {
-        if(this.shotCheckCounter >= 0) {
+      if (window.isKeyDown.key_z === true) {
+        if (this.shotCheckCounter >= 0) {
           let i;
 
-          for(i = 0; i < this.shotArray.length; ++i) {
-            if(this.shotArray[i].life <= 0) {
+          for (i = 0; i < this.shotArray.length; ++i) {
+            if (this.shotArray[i].life <= 0) {
               this.shotArray[i].set(this.position.x, this.position.y);
               this.shotCheckCounter = -this.shotInterval;
               break;
             }
           }
 
-          for(i = 0; i < this.singleShotArray.length; i += 2) {
+          for (i = 0; i < this.singleShotArray.length; i += 2) {
+            if (
+              this.singleShotArray[i].life <= 0 &&
+              this.singleShotArray[i + 1].life <= 0
+            ) {
+              let radCW = (280 * Math.PI) / 180;
+              let radCCW = (260 * Math.PI) / 180;
 
+              this.singleShotArray[i].set(this.position.x, this.position.y);
+              this.singleShotArray[i].setVectorFromAngle(radCW);
+              this.singleShotArray[i + 1].set(this.position.x, this.position.y);
+              this.singleShotArray[i + 1].setVectorFromAngle(radCCW);
+
+              this.shotCheckCounter = -this.shotInterval;
+              break;
+            }
           }
         }
       }
+
+      ++this.shotCheckCounter;
     }
+    this.draw();
+
+    this.ctx.globalAlpha = 1.0;
+  }
+}
+
+class Shot extends Character {
+  constructor(ctx, x, y, w, h, imagePath) {
+    super(ctx, x, y, w, h, 0, imagePath);
+
+    this.speed = 7;
+  }
+
+  set(x, y) {
+    this.position.set(x, y);
+    this.life = 1;
+  }
+
+  update() {
+    if (this.life <= 0) {
+      return;
+    }
+    if (this.position.y + this.height < 0) {
+      this.life = 0;
+    }
+
+    this.position.x += this.vector.x * this.speed;
+    this.position.y += this.vector.y * this.speed;
+
+    this.rotationDraw();
   }
 }
